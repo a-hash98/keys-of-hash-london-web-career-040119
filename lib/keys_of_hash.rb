a3bb386ebb97da8a3755c615316fa5d6d9c1f581
Ay Has <ahashim983@yahoo.com>
2019-04-01 15:04:12 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,13 +2,7 @@ class Hash
 
   def keys_of(arguments)
     arr = []
-    hash.each |key,value| do
-      if value == arguments
-        arr.push(value)
-      end
-    end
-    return arr
-  end
+    for item in 
 
  return
 end
